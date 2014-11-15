dba1c4b2dae9ee6a056c85ef08451e884375753d
Alexander <duff1994@gmail.com>
2014-11-15 16:37:45 +0200

    Added test files

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,10 +7,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //Map<Character, Integer> oneMap = HuffmanFileWrapper.WrapFile("C:\\Users\\Davydov\\Documents\\file with frequenciesUTF8.txt");
-        //Map<Character, Integer> oneMap = HuffmanFileWrapper.WrapFile("C:\\Users\\Davydov\\Documents\\file with frequenciesASCII.txt");
-        //Map<Character, Integer> oneMap = HuffmanFileWrapper.WrapFile("C:\\Users\\Davydov\\Documents\\The Hunger Games.txt");
-        Map<Character, Integer> oneMap = HuffmanFileWrapper.WrapFile("C:\\Users\\Davydov\\Documents\\testwithutf8.txt");
+        String currentDir = System.getProperty("user.dir");
+
+        Map<Character, Integer> oneMap = HuffmanFileWrapper.WrapFile(currentDir+"\\The Hunger Games.txt");
+        //Map<Character, Integer> oneMap = HuffmanFileWrapper.WrapFile(currentDir + "\\testwithutf8.txt");
+        //Map<Character, Integer> oneMap = HuffmanFileWrapper.WrapFile(currentDir+"\\file with frequenciesASCII.txt");
+        //Map<Character, Integer> oneMap = HuffmanFileWrapper.WrapFile(currentDir+"\\file with frequenciesUTF8.txt");
 
         for (Map.Entry entry : oneMap.entrySet()) {
             System.out.println(entry.getKey() + " - " + entry.getValue());
